23cdbb4843da6526c017d75d4c70ddfd7cd78b87
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2023-03-04 19:27:10 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/NumberRange.pm
@@ -0,0 +1,25 @@
+package Acme::CPANModules::NumberRange;
+
+use strict;
+
+use Acme::CPANModulesUtil::Misc;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+our $LIST = {
+    summary => "List of modules that handle number ranges",
+    description => <<'_',
+
+
+_
+};
+
+Acme::CPANModulesUtil::Misc::populate_entries_from_module_links_in_description;
+
+1;
+# ABSTRACT:
+
+=head1 DESCRIPTION
